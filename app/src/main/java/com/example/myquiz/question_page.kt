6be5c33318b411cc712194b14a590d7488e4a422
azutsavv <azutsav2002@gmail.com>
2022-09-13 19:30:22 +0530

    quiz_app

--- app/src/main/java/com/example/myquiz/question_page.kt
+++ app/src/main/java/com/example/myquiz/question_page.kt
@@ -2,10 +2,16 @@ package com.example.myquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class question_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_page)
+        set_data.questions()
+
+        var demo = set_data.questions()
+        Log.e("My Test", "${demo.size}")
     }
+
 }
